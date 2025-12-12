3d2b57786d9f93a4edd4ba907d1aa4f793bfd06b
Brendan Hendrata <bhendrata@cmu.edu>
2025-12-12 14:15:04 -0500

    updated interface

--- participant.sv
+++ participant.sv
@@ -1,16 +1,26 @@
 module participant #(
-    parameter ADDR_WIDTH = 7,
-    parameter DATA_WIDTH = 8
+    parameter int ADDR_WIDTH = 7,
+    parameter int DATA_WIDTH = 8
 ) (
-    input  logic                     clock,
-    input  logic                     reset,
-    inout  logic                     sda,
-    inout  logic                     scl,
+    input  logic clock, reset,
+
+    // Shared I2C lines
+    inout  tri sda, scl,
+
+    // Participant interface
     input  logic  [ADDR_WIDTH-1:0]   own_addr,
+
+    // Reading
     output logic  [DATA_WIDTH-1:0]   rx_data,
     output logic                     valid,
+
+    // Writing
     input  logic [DATA_WIDTH-1:0]    tx_data,
     input  logic                     ready,
+
+    // Status
+    output logic                     busy,
     output logic                     ack_error
-);
+); 
+    
 endmodule: participant
